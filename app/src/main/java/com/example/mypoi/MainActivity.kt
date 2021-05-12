a816ff685384dfc88946c41f2ddb368000c3a3f8
MirkoGiuliano <64957560+MirkoGiuliano@users.noreply.github.com>
2021-05-12 23:17:07 +0200

    creazione activity per aggiungere una nuova posizione

--- app/src/main/java/com/example/mypoi/MainActivity.kt
+++ app/src/main/java/com/example/mypoi/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.ArrayList
 import com.example.mypoi.DatoLista as DatoLista
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       val add_new_posizione: Button = findViewById(R.id.add_new_posizione)
+       val add_new_posizione: FloatingActionButton = findViewById(R.id.add_new_posizione)
         add_new_posizione.setOnClickListener{
             val intent = Intent(this, Activity_registra_POI::class.java)
                     startActivity(intent)
